3b6b4209a874e21c0df4bdf582781247915d9e56
k4pran <k4pran@gmail.com>
2024-12-07 10:27:59 +0000

    2024 day7 - fix day print

--- 2024/day7/p2.py
+++ 2024/day7/p2.py
@@ -32,26 +32,12 @@ with open("day7.txt", 'r') as f:
         result, nums = line.split(": ")
         result = int(result)
         nums = [int(x) for x in nums.split(" ")]
-        # todo why this not work? oh because 5 + 5 * 5 = even
-        # odd is 2k + 1
-        # (2a + 1) + (2b + 1) = 2a + 2b + 2 = 2(a + b + 1) = 2k where k = a + b + a -> always even +. that's why it failed!
-        # (2a + 1) * (2b + 1) = 4ab + 2a + 2b + 1 = 2(2ab + a + b) + 1 = 2k + 1 where k = 2ab + a + b -> always odd
-        # if is_even(result) and all([not is_even(x) for x in nums]):
-        #     continue
-
-        # I think below make sense - 4 * 4 + 2 = even. x = 2k.
-        # 2a + 2b = 2(a + b) = 2k where k = (a + b) -> always even
-        # 2a * 2b = 2(a * b) = 2k where k = (a * b) -> always even
         if not is_even(result) and all([is_even(x) for x in nums]):
             continue
         if result in get_combos(nums):
             total += result
 
 
-    print(f"Day 7-1: {total}")
-
-    # nope - 1985078235904 - too low (I think)
-    # nope - 1985268803998 - too high
-    # 1985268524462 - YUP
+    print(f"Day 7-2: {total}")
 
 
