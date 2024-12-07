4754bad612d4882dd13bd49a26b4e1045f4bcb6a
k4pran <k4pran@gmail.com>
2024-12-07 10:43:05 +0000

    2024 day7 - tiny optimization

--- 2024/day7/p2.py
+++ 2024/day7/p2.py
@@ -1,5 +1,4 @@
-from collections import defaultdict
-
+import time
 operators = ('+', '*')
 memo = dict()
 
@@ -7,11 +6,13 @@ def is_even(num):
     return num % 2 == 0
 
 
-def get_combos(nums):
+def get_combos(nums, result):
     possibilities = list(nums[:1])
     for num in nums[1:]:
         updated = []
         for possibility in possibilities:
+            if possibility > result:
+                continue
             r1 = possibility * num
             r2 = possibility + num
             r3 = int(str(possibility) + str(num))
@@ -20,11 +21,11 @@ def get_combos(nums):
             updated.append(r3)
 
         possibilities = updated
-    print(possibilities)
     return possibilities
 
 
 with open("day7.txt", 'r') as f:
+    start = time.time()
     lines = f.read().splitlines()
 
     total = 0
@@ -34,10 +35,13 @@ with open("day7.txt", 'r') as f:
         nums = [int(x) for x in nums.split(" ")]
         if not is_even(result) and all([is_even(x) for x in nums]):
             continue
-        if result in get_combos(nums):
+        if result in get_combos(nums, result):
             total += result
 
 
     print(f"Day 7-2: {total}")
+    end = time.time()
+    print(f"Time taken: {end - start}")
+    # 1.6867587566375732 :(
 
 
